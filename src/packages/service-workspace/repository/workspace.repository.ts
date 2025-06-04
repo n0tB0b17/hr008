46a15a693f4f64f594aaf852cf9ad2af3cdde269
n0tB0b17 <bob1.khanal7@gmail.com>
2025-06-05 02:28:29 +0545

    added repository enhancement

--- src/packages/service-workspace/repository/workspace.repository.ts
+++ src/packages/service-workspace/repository/workspace.repository.ts
@@ -5,6 +5,7 @@ import { IWorkspace } from "@/packages/core/interface/iworkspace.interface";
 import { UpdateWorkspaceDto } from "@/packages/core/dtos/update-workspace.dto";
 import { WorkspaceMemberRole } from "@/packages/core/enums/workspace-member-role.enum";
 import { IWorkspaceMember } from "@/packages/core/interface/iworkspacemember.interface";
+import { NotFoundError } from "@/packages/core/errors/custom-error";
 
 
 
@@ -38,11 +39,19 @@ class WorkspaceRepository implements IworkspaceRepository {
     }
 
     async findById(workspaceId: string): Promise<IWorkspace | null> {
-        return null;
+        const resp = await this.prisma.workspace.findUnique({
+            where: { workspaceId }
+        })
+
+        return resp ? toDomainInWorkspace(resp) : null;
     }
 
+    // find workspace by ownerId
     async findByUserId(ownerId: string): Promise<IWorkspace[]> {
-        return []
+        const resp = await this.prisma.workspace.findMany({
+            where: { ownerId }
+        })
+        return resp.map(toDomainInWorkspace);
     }
 
     async delete(workspaceId: string): Promise<boolean> {
@@ -50,28 +59,105 @@ class WorkspaceRepository implements IworkspaceRepository {
     }
 
     async update(workspaceId: string, docs: Partial<UpdateWorkspaceDto>): Promise<IWorkspace | null> {
-        return null;
+        const dataToUpdate: any = {};
+        if (docs.name !== undefined) dataToUpdate["name"] = docs.name;
+        if (docs.description !== undefined) dataToUpdate["description"] = docs.description;
+        if (docs.visibility !== undefined) dataToUpdate["visibility"] = docs.visibility as any;
+
+        if (Object.keys(dataToUpdate).length === 0) {
+            return this.findById(workspaceId);
+        }
+
+        const resp = await this.prisma.workspace.update({
+            where: { workspaceId },
+            data: dataToUpdate,
+        })
+
+        return toDomainInWorkspace(resp);
     }
 
 
     async addMember(workspaceId: string, userId: string, role: WorkspaceMemberRole): Promise<IWorkspaceMember | null> {
-        return null;
+        try {
+            const resp = await this.prisma.workspaceMember.create({
+                data: {
+                    workspaceId,
+                    userId,
+                    role: role as any
+                }
+            })
+
+
+            return toDomainInWorkspaceMember(resp);
+        } catch (e: any) {
+            if (e.code === "P2002") {
+                return this.findMember(workspaceId, userId);
+            }
+
+            console.error(`Error added member to workspace`);
+            return null;
+        }
     }
 
     async findMember(workspaceId: string, userId: string): Promise<IWorkspaceMember | null> {
-        return null;
+        const resp = await this.prisma.workspaceMember.findUnique({
+            where: {
+                workspaceId_userId: {
+                    workspaceId,
+                    userId
+                }
+            }
+        })
+
+
+        return resp ? toDomainInWorkspaceMember(resp) : null;
     }
 
     async getMembers(workspaceId: string): Promise<IWorkspaceMember[]> {
-        return [];
+        const resp = await this.prisma.workspaceMember.findMany({
+            where: {
+                workspaceId
+            }
+        });
+        return resp.map(toDomainInWorkspaceMember);
     }
 
     async updateMemberRole(workspaceId: string, userId: string, role: WorkspaceMemberRole): Promise<IWorkspaceMember | null> {
-        return null;
+        const reps = await this.prisma.workspaceMember.update({
+            where: {
+                workspaceId_userId: {
+                    workspaceId,
+                    userId,
+                }
+            },
+            data: {
+                role: role as any
+            }
+        });
+
+        return reps ? toDomainInWorkspaceMember(reps) : null;
     }
 
     async removeMember(workspaceId: string, userId: string): Promise<boolean> {
-        return false;
+        try {
+            await this.prisma.workspaceMember.delete({
+                where: {
+                    workspaceId_userId: {
+                        workspaceId,
+                        userId,
+                    }
+                }
+            })
+
+            return true;
+        } catch (e: any) {
+            if (e.code === "P2025") {
+                throw new NotFoundError("not found...");
+            }
+            
+            console.error(`error while deleting member from workspace`);
+            return false;
+        }
     }
 }
 
